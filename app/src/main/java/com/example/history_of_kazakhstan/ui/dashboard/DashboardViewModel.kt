49eadb1b51f6323b2636d32897b5e1b37e246cee
o1a2h <bazbazolol188@gmail.com>
2024-11-23 22:02:13 +0500

    Start

---
+++ app/src/main/java/com/example/history_of_kazakhstan/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.history_of_kazakhstan.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
